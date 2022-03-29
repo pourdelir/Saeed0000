a8fe70d56b27d68c0aa5e1bdf97dc8a4dc7a5893
SAEED <Saeid.poordelir@gmail.com>
2022-03-29 20:46:07 +0100

    Start of the working project - No extra feature

---
+++ RTXB/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+
+namespace RTXB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Read TXT");
+            using (StreamReader sr = new StreamReader(@"E:\Download\pwned-passwords-sha1-ordered-by-count-v8\pwned-passwords-sha1-ordered-by-count-v8.txt"))
+            {
+
+                String line;
+                // Read line by line 
+                int row = 1;
+
+                    // while ((line = sr.ReadLine()) != null) 
+                    while (row<100) 
+
+                {
+                    line = sr.ReadLine();
+                    Console.WriteLine(line);
+                    row++;
+
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
