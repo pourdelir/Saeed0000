9914cc67f1bedbc36a21d96c6e7a5eeee8ba1597
SAEED <Saeid.poordelir@gmail.com>
2022-04-05 18:07:00 +0100

    Ready for Remote

--- RTXB/Program.cs
+++ RTXB/Program.cs
@@ -9,6 +9,10 @@ namespace RTXB
         static void Main(string[] args)
         {
             Console.WriteLine("Read TXT");
+            Console.WriteLine("Enter name and path of the text file")
+            Console.WriteLine("How many lines of the text file you'd like to stream? 0 : entire file.");
+            int lines = Convert.ToInt16( Console.ReadLine());
+            
             using (StreamReader sr = new StreamReader(@"E:\Download\pwned-passwords-sha1-ordered-by-count-v8\pwned-passwords-sha1-ordered-by-count-v8.txt"))
             {
 
